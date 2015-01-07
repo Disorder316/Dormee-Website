792764eca666e6db598d024c71b6e3451f2cecd0
Jonathan Mann <ironyisjust@gmail.com>
2015-01-07 02:11:19 -0600

    Revert "Removed the gh-pages autofill"
    This reverts commit dd07cc0e69596dd69141405393690ae4c7f5e75b.

---
+++ index.php
@@ -0,0 +1,473 @@
+<DOCTYPE html>
+<html>
+<head>
+<!-- Oswald Google Font -->
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
+<!-- Lato Google Font -->
+<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<!-- Main CSS File -->
+<link rel="stylesheet" href="css/main.css">
+<!-- The Title -->
+<title>Dormee Music</title>
+</head>
+<body>
+
+<div id="top-level">
+	<!--	<div class="container">
+			<div class="row">
+				<img class="logo" src="img/top-level-logo.jpg">
+				<h1>DORMEE</h1>
+				<h3>IT CAME FROM THE 80's</h3>
+			</div>
+		</div>  -->
+</div>
+<header>
+	<nav id="myAffix" class="navbar">
+		<div class="container col-md-8 col-md-offset-2">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">
+					<p>DORMEE</p>
+				</a>
+			</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#the-band" >The Band</a></li>
+					<li><a href="#videos" >Videos</a></li>
+					<li><a href="#upcoming-events" >Events</a></li>
+					<li><a href="#gallery" >Gallery</a></li>
+					<li><a href="#contact" >Contact</a></li>
+				</ul>
+		</div>	
+	</nav>
+
+</header>
+<main class="home">
+	<section id="the-band">
+		<div class="container">
+			<div class="row">
+				<h2>The Band</h2>
+				<p>I am a classically trained Electronic Dance Music artist, producer, and Dj. I started formal training as a classical pianist and violinist at a young age and launched myself into the performance arena, performing numerous classical concerts throughout the Midwestern United States, winning competitions and performing with numerous orchestras as featured artist. I studied piano and violin performance at university but decided to drop my classical music career and create my own studio in 2014 as a collaborative EDM production studio working with local and international artists to be featured in my tracks. My style ranges from '90s House and Hip Hop to the grinding bass driven genres of Electro House and Dubstep.</p>
+				<img id="thumb-1" src="img/Jonny.jpg" class="img-rounded" alt="Rounded Image">
+				<img id="thumb-2" src="img/gallery-12.jpg" class="img-rounded" alt="Rounded Image">
+				<img id="thumb-3" src="img/member-3.jpg" class="img-rounded" alt="Rounded Image">
+				<img id="thumb-4" src="img/member-4.jpg" class="img-rounded" alt="Rounded Image">
+			</div>
+				<div class="row active thumb-1">
+					<div class="slider col-md-10 col-md-offset-1">
+						<div class="slider-img col-md-5">
+							<img src="img/Jonny.jpg">
+						</div>
+						<div class="slider-txt col-md-7">
+							<h4>JONNY REESE</h4>
+							<h5>PRODUCER</h5>
+							<p>Dormee Producer</p>
+						</div>		
+					</div>
+				</div>
+				<!-- Hidden Until Clicked -->
+				<div class="row hidden thumb-2">
+					<div class="slider col-md-10 col-md-offset-1">
+						<div class="slider-img col-md-5">
+							<img src="img/gallery-12.jpg">
+						</div>
+						<div class="slider-txt col-md-7">
+							<h4>Batmann</h4>
+							<h5>DJ</h5>
+							<p>Spins it like there's no tomorrow</p>
+						</div>		
+					</div>				
+				</div>
+				<!-- Hidden Until Clicked -->
+				<div class="row hidden thumb-3">
+					<div class="slider col-md-10 col-md-offset-1">
+						<div class="slider-img col-md-5">
+							<img src="img/member-3.jpg">
+						</div>
+						<div class="slider-txt col-md-7">
+							<h4>ARTIST NAME HERE</h4>
+							<h5>FEATURED ARTIST</h5>
+							<p>Morbi rutrum, elit ac fermentum egestas, tortor ante vestibulum est, eget scelerisque nisl velit eget tellus. Lorem dolor amet, consectetuer adipiscing elit.</p>
+						</div>		
+					</div>				
+				</div>
+				<!-- Hidden Until Clicked -->
+				<div class="row hidden thumb-4">
+					<div class="slider col-md-10 col-md-offset-1">
+						<div class="slider-img col-md-5">
+							<img src="img/member-4.jpg">
+						</div>
+						<div class="slider-txt col-md-7">
+							<h4>ARTIST NAME HERE</h4>
+							<h5>FEATURED ARTIST</h5>
+							<p>Morbi rutrum, elit ac fermentum egestas, tortor ante vestibulum est, eget scelerisque nisl velit eget tellus. Lorem dolor amet, consectetuer adipiscing elit.</p>
+						</div>		
+					</div>				
+				</div>
+				<!-- End Hidden Classes-->
+			</div>
+		</div>
+	</section>
+
+	<section id="videos">
+		<div class="container">
+			<div class="row">
+				<h2>VIDEOS</h2>
+				<p>Check out some of our amazing music videos. Don't forget to like and subscribe to the youtube channel!</p>
+							<!--	<img class="video_player" src="image.jpg" id="player"></img>
+								<button onclick="goFullscreen('player'); return false">Click Me To Go Fullscreen! (For real)</button> -->
+			</div>
+			<div class="row spacer">
+					<div class="col-sm-4 video-thumb">
+						<img class="youtube" src="//img.youtube.com/vi/vKNLQFEzp4g/0.jpg" id="vKNLQFEzp4g" data-toggle="modal" data-target="#myModal">
+					</div>
+					<div class="col-sm-4 video-thumb">
+						<img class="youtube" src="//img.youtube.com/vi/ZsziXzK57tg/0.jpg" id="ZsziXzK57tg" data-toggle="modal" data-target="#myModal">
+			
+					</div>
+					
+					<div class="col-sm-4 video-thumb">
+						<img class="youtube" src="//img.youtube.com/vi/vS04ENeJ5w0/0.jpg" id="vS04ENeJ5w0" data-toggle="modal" data-target="#myModal">
+					</div>
+			</div>
+			
+			<div class="row">
+					<div class="col-sm-4 video-thumb">
+						<img class="youtube" src="//img.youtube.com/vi/ou5hk9w_lfk/0.jpg" id="ou5hk9w_lfk" data-toggle="modal" data-target="#myModal">
+					</div>
+				
+					<div class="col-sm-4 video-thumb">
+						<img class="youtube" src="//img.youtube.com/vi/iGb6aCbJeI8/0.jpg" id="iGb6aCbJeI8" data-toggle="modal" data-target="#myModal">
+					</div>
+					
+					<div class="col-sm-4 video-thumb">
+						<img class="youtube" src="//img.youtube.com/vi/vKNLQFEzp4g/0.jpg" id="vKNLQFEzp4g" data-toggle="modal" data-target="#myModal">
+					</div>
+			</div>
+		</div>
+	</section>
+		
+	<section id="upcoming-events">
+		<div class="container">
+			<div class="row">
+				<h2>UPCOMING EVENTS</h2>
+				<p>Dormee performs as often as possible and we want you to be at our next show. This is our upcoming event's listing. Come check out our show, and if there's an open date feel free to contact me for booking information. </p>
+			</div>
+			
+			<div class="row spacer">
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+				
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+				
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+				
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+			
+			</div>
+			
+			<div class="row">
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+				
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+				
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+				
+				<div class="events col-md-3">
+					<div class="event-date">
+						<h3>22 FEB</h3>
+					</div>
+					
+					<div class="event-info">
+						<h4 class="event-name">EVENT NAME</h4>
+						<p>Morbi rutrum, elit ac ofei fermentum egestas, kune tortor ante vestibulum ik est, eget scelerisque nisl velit eget tellus.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section id="gallery">
+		<div class="container">
+			<div class="row">
+				<h2>GALLERY</h2>
+				<p>Awesome pictures of some great times. Check em' out, but next time try to be there in person so you can make new memories with us going forward.</p>
+			</div>
+			<div class="row spacer">
+				<div class="gallery-images col-md-3">
+				<a href="#" class="thumbnail">
+					<img src="img/gallery-1.jpg">
+				</a>	
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-2.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-3.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-4.jpg">
+					</a>
+				</div>
+			</div>	
+			
+			<div class="row">
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-5.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-6.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-7.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-8.jpg">
+					</a>
+				</div>
+			</div>	
+			
+			<div class="row">
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-9.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-10.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-11.jpg">
+					</a>
+				</div>
+				
+				<div class="gallery-images col-md-3">
+					<a href="#" class="thumbnail">
+					<img src="img/gallery-12.jpg">
+					</a>
+				</div>
+			</div>	
+		</div>
+	</section>
+	
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<h2>CONTACT</h2>
+				<p>If you free music, information, or just want to get in touch with Dormee all you have to do is fill out the beautiful form below. We will try to respond to any questions as soon as possible. Enjoy the music!</p>
+			</div>
+			<div class="row spacer col-md-12">
+				<div class="col-md-6">
+							<form method="post" action="">
+						        
+						    <label>Name:</label><br>
+						    <input id="name" name="name" placeholder="Your Name" type="text"><br>
+						            
+						    <label>Email:</label><br>
+						    <input name="email" id="email" placeholder="name@email.com" type="email"><br>
+						            						      
+						    <label>Message:</label><br>
+						    <textarea name="message" id="message" placeholder="Add a brief message..."></textarea><br>
+						            
+						    <input id="submit" name="submit" value="Submit" type="submit"><br>
+						        
+</form>
+				</div>
+				<div class="col-md-6">
+					<!-- <ul id="contact-list">
+						<li><span>Office :</span> Venue of the night!</li>
+						<li><span>Phone :</span> 213-421-2321</li>
+						<li><span>Fax :</span> 123-432-1234</li>
+						<li><span>Email :</span> admin@website.com</li>
+					</ul> -->
+					<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/98596630&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+				</div>
+			</div>	
+		</div>
+	</section>
+	<section id="the-modal">
+		<div id="myModal" class="modal fade">
+		  <div class="modal-dialog">
+			<div class="modal-content">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Dormee Music</h4>
+			  </div>
+			  <div class="modal-body">
+				<iframe class="modal-iframe" width="560" height="315" src="//www.youtube.com/embed/vKNLQFEzp4g?autoplay=1" frameborder="0" allowfullscreen></iframe>
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			  </div>
+			</div><!-- /.modal-content -->
+		  </div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
+	</section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<h2>DORMEE</h2>
+				<p id="social-icons">
+					<a href="https://www.facebook.com/DJDormee"><img src="img/facebook-square.png"></a>
+					<a href="https://soundcloud.com/dormee/"><img src="img/soundcloud.png"></a>
+					<a href="https://twitter.com/DormeeMusic"><img src="img/twitter.png"></a>
+				</p>
+				<p id="copywrite">Copyright 2014. All Rights Reserved.</p>
+			</div>
+		</div>
+	</footer>
+</main>
+<!-- Jquery Oh Yeah!-->
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+
+<script>
+$(document).ready(function() {
+	<!-- Gallery Prevent Default -->
+	$("#gallery a").click(function(e){
+		e.preventDefault();
+	});
+	
+	<!-- Navigation Script Final -->
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+	
+	<!-- Profile Script Final -->
+	$('#the-band img.img-rounded').hover(function(){
+		$('.thumb-1, .thumb-2, .thumb-3, .thumb-4').removeClass('active').addClass('hidden');
+		$('.' + this.id).removeClass('hidden').addClass("active");
+		$('.hidden').hide();
+		$('.active').fadeIn('slow');
+	});
+	
+	
+	<!-- Youtube Modal Script Final -->
+	$('img.youtube').click(function() {
+		$('.modal-title').text("Dormee Music Videos");
+		$('.modal-iframe').replaceWith('<iframe class="modal-iframe" width="560" height="315" src="//www.youtube.com/embed/'+ this.id + '?autoplay=1" frameborder="0" allowfullscreen></iframe>')
+	}); 
+	/* $("img.gallerythumb").click(function() {
+		theImage = ($(this).attr('src'));
+		$('.modal-title').text("Dormee Gallery");
+		$('.modal-iframe').replaceWith('<img src="' + theImage + ' "style="height: auto; width: 560px;"></img>');
+	});*/ 
+	
+
+	
+	<!-- Navbar Fixed Script Final -->
+	// Selects ID to be affixed and calculates offset to adjust for screen height. 
+	$('#myAffix').affix({
+        offset: { top: $('#myAffix').offset().top }
+    });
+	//Selects the element being affixed and runs a function adding margin to fill the void after the Nav Fixes to the top.
+	$('nav').on('affix.bs.affix', function () {
+		$('#top-level').css('margin-top', '100px');
+	});
+	//Removes the added margin when the navbar re-enters the DOM.
+	$('nav').on('affix-top.bs.affix', function () {
+		$('#top-level').css('margin-top', '0px');
+	});
+});
+</script>
+</body>
+</html>
